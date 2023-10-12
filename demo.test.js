8c29604009250be9a35ac180df773c19bd384bab
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-10-12 19:36:52 +0200

    add describe

--- demo.test.js
+++ demo.test.js
@@ -1,13 +1,18 @@
 /**
  * ecrire un test simple
  */
-test ('demo somme',function(){
 
-    const a= 2+2
-    expect(a).toBe(4)
-})
 
-it('demo somme 2',function(){
-    const b=3+3
-    expect(b).toBe(6)
+describe('pack démo', function(){
+   
+    test ('demo somme',function(){
+
+        const a= 2+2
+        expect(a).toBe(4)
+    })
+    
+    it('demo somme 2',function(){
+        const b=3+3
+        expect(b).toBe(6)
+    })
 })
