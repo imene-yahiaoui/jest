db4c8ca1d645f3719e6e75c218be42664dab1a47
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-10-13 20:20:19 +0200

    add test from export

--- src/demo.test.js
+++ src/demo.test.js
@@ -2,17 +2,26 @@
  * ecrire un test simple
  */
 
-
 describe('pack démo', function(){
-   
+
     test ('demo somme',function(){
 
         const a= 2+2
         expect(a).toBe(4)
     })
-    
+
     it('demo somme 2',function(){
         const b=3+3
         expect(b).toBe(6)
     })
 })
+
+/**
+ * autre moyene
+ */
+const demo = require("./demo");
+
+test("demo() addition", () => {
+  const result = demo(1, 2)
+  expect(result).toBe(3)
+});
