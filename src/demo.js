395c54c35b766185e3575daa3079ce1bc14a08c2
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-10-20 16:02:14 +0200

    add test with many factories, but I don't know how to match them

--- src/demo.js
+++ src/demo.js
@@ -1,10 +1,20 @@
 module.exports = (a, b) => a + b;
 
 /**
- * function with many factories
+ *I have a function with many factories, I  know how to match them
  */
 const sum = (a, b, c) => {
   return (a || null) + (b || null) + (c || null);
 };
 
 module.exports = sum;
+
+/**
+ *I have a function with many factories, but I don't know how to match them
+ */
+
+const theSum = (...numbers) => {
+  return numbers.reduce((pv, cv) => pv + cv, 0);
+  //ps: jai ajouté ',0' en cas ou ya pas des numero il va me retourner 0
+};
+module.exports = theSum;
--- src/testes/demo.test.js
+++ src/testes/demo.test.js
@@ -30,9 +30,24 @@ it("should retourn 7", function(){
     const a= 8
     expect(a).toBe(8)
 })
+/**
+ *I have a function with many factories, I  know how to match them
+ */
 const sum = require("../demo");
 
 it("should retourn sum",()=>{
     console.log(sum(10,10,40))
     expect(sum(10)).toBe(10)
+})
+/**
+ *I have a function with many factories, but I don't know how to match them
+ */
+ const theSum = require("../demo");
+ it("should retourn sum",()=>{
+    console.log(theSum(10,10,400))
+    expect(theSum(100,1,10,40,50)).toBe(201)
+})
+ 
+it("should retourn sum",()=>{
+   expect(theSum()).toBe(0)
 })
